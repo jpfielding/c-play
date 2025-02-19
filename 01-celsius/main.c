ec957758f4a5cbd49821baa1ada738c52cff94d0
JP Fielding <jp.fielding@gmail.com>
2025-02-19 01:08:51 +0000

    temps as floats

--- 01-celsius/main.c
+++ 01-celsius/main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 /* print farhrenheit table for fahr = 0,220, ... 300 */
-int main() {
+int main()
+{
 
-    float fahr, celsius; 
+    float fahr, celsius;
     float lower, upper, step;
 
-    lower = 0; /* lower limit of temp scale */
+    lower = 0;   /* lower limit of temp scale */
     upper = 300; /* upper limit */
-    step = 20; /* step size */
+    step = 20;   /* step size */
 
     fahr = lower;
-    while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
+    while (fahr <= upper)
+    {
+        celsius = 5 * (fahr - 32) / 9;
         printf("temp: %3.0ff, %6.1fc\n", fahr, celsius);
         fahr = fahr + step;
     }
