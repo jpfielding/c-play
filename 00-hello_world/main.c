6fc2e75db31a8bf2be2f795953ec51e0b271a5b7
JP Fielding <jp.fielding@gmail.com>
2025-10-24 00:34:31 +0000

    basic input

--- 00-hello_world/main.c
+++ 00-hello_world/main.c
@@ -9,10 +9,13 @@ int main()
     int age;
 
     printf("Enter your name: ");
-    scanf("%24s", name); //limit input to avoid overflow
+    // scanf("%24s", name); //limit input to avoid overflow
+    fgets(name, sizeof(name), stdin); // safer way to read strings
+    name[strcspn(name, "\n")] = 0; // remove newline character
+    
     printf("Enter your age: ");
     scanf("%d", &age);
     printf("Hello %s, you are %d years old!\n", name, age);
-    
+
     return 0;
 }
